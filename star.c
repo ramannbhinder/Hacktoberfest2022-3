a4e877a01ba341a7a78a9ee52751b6cbd02c6bcf
ramannbhinder <113259189+ramannbhinder@users.noreply.github.com>
2022-10-09 02:24:00 +0530

    Create star.c

---
+++ star.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,rows,columns, a[5][5], b[5][5],count=1;
+    printf("Enter the number of rows and columns : \n");
+    scanf("%d %d", &i,&j);
+    
+    printf("Enter the elements\n");
+    for(rows=0;rows <i; rows++)
+    {
+         for(columns=0;columns<j;columns++)
+         {
+            for(columns=0;columns<j;columns++)
+            {
+                scanf("%d", &a[rows][columns]);
+            }
+         }
+         for()
+
+    }
+}
